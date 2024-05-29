0d4a252c7a32aa18a9fe04eb2625031f76f83b61
zuk246 <tadasukeiizuka@gmail.com>
2024-05-29 19:21:30 +0900

    add: image changing feature #7

--- src/features/display/index.tsx
+++ src/features/display/index.tsx
@@ -1,36 +1,41 @@
-import BackgroundImage from './../../assets/background/image.webp';
-import BackgroundContent from './../../assets/background/content.webp';
-import { memo } from 'react';
+import { useCookies } from 'react-cookie';
+import { memo, useEffect, useState } from 'react';
+import { images } from '../../data/images';
 
 type Props = {
     hours: number;
     minutes: number;
     seconds: number;
-    blur: Boolean;
+    blur: boolean;
 };
+
+function selectBg(id: string) {
+    const res = images.filter((image) => image.id === id)[0];
+    return res.url;
+}
 
 const Display = memo((props: Props) => {
     const { hours, minutes, seconds } = props;
+    const [cookies] = useCookies();
+    const [background, setBackground] = useState<string>(selectBg('black'));
 
     const COMMON_CLASS_NAME_NUMBER =
-        'w-40 text-center bg-clip-text text-transparent bg-white/40 max-[1200px]:w-[calc(0.25rem*35)] max-[990px]:w-[calc(0.25rem*27)] max-[810px]:w-[calc(0.25rem*20)] max-[560px]:w-[calc(0.25rem*14)] max-[375px]:w-[calc(0.25rem*8)]';
+        'w-40 text-center bg-clip-text text-transparent bg-white/60 max-[1200px]:w-[calc(0.25rem*35)] max-[990px]:w-[calc(0.25rem*27)] max-[810px]:w-[calc(0.25rem*20)] max-[560px]:w-[calc(0.25rem*14)] max-[375px]:w-[calc(0.25rem*8)]';
     const COMMON_CLASS_NAME_COLON =
         'w-28 text-center bg-clip-text text-transparent bg-white/40 max-[1200px]:w-16 max-[990px]:w-[calc(0.25rem*14)] max-[810px]:w-[calc(0.25rem*10)] max-[560px]:w-[calc(0.25rem*6)] max-[375px]:w-[calc(0.25rem*4)]';
+
+    useEffect(() => {
+        if (images.map((image) => image.id).includes(cookies.background)) {
+            setBackground(selectBg(cookies.background));
+        }
+    }, [cookies.background]);
 
     return (
         <div>
             <img
-                src={BackgroundImage}
+                src={background}
                 alt=''
                 className='fixed top-0 left-0 w-full h-[100svh] object-cover object-center z-0 duration-1000'
-                style={{
-                    filter: `${!props.blur ? 'blur(0px)' : 'blur(8px)'}`,
-                }}
-            />
-            <img
-                src={BackgroundContent}
-                alt=''
-                className='fixed top-0 left-0 w-full h-[100svh] object-cover object-center z-20 duration-1000'
                 style={{
                     filter: `${!props.blur ? 'blur(0px)' : 'blur(8px)'}`,
                 }}
